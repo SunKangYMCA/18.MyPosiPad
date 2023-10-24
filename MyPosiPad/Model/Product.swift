ae2a0c3f2e6ae071446b435804429a13ea7113cb
SunKang <coolshark248@gmail.com>
2023-10-23 22:44:55 -0600

    aa

--- MyPosiPad/Model/Product.swift
+++ MyPosiPad/Model/Product.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 
-struct Product: Identifiable, Hashable {
+struct Product: Codable, Identifiable, Hashable {
     var id: UUID = UUID()
     var name: String
     var price: Double
     var quantity: Int
     var smallPicture: String
     var largePicture: String
+    var type: ProductType
     var size: String
     var color: String
 }
